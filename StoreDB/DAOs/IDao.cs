7215787e7b6b5f666bc117ef01065b082bb00860
LucasTran380381 <trannhan380381@gmail.com>
2020-10-29 23:20:29 +0700

    interface Dao

---
+++ StoreDB/DAOs/IDao.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace StoreDB.DAOs
+{
+    public interface IDao<T>
+    {
+        public List<T> GetAll();
+
+        public T Get(string id);
+
+        public bool Save(T t);
+
+        public bool Update(T t);
+
+        public bool Delete(T t);
+    }
+}
