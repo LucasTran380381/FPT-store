1f49f9408c6331b43c2eda95bd75e67d5d37de9f
LucasTran380381 <trannhan380381@gmail.com>
2020-10-29 23:40:46 +0700

    db util

--- StoreDB/DAOs/IDao.cs
+++ StoreDB/DAOs/IDao.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Dynamic;
 
 namespace StoreDB.DAOs
 {
---
+++ StoreDB/Util/DbUtil.cs
@@ -0,0 +1,12 @@
+namespace StoreDB.Util
+{
+    public class DbUtil
+    {
+        private const string ConnString = "";
+        
+        public static void GetConn()
+        {
+            
+        }
+    }
+}
