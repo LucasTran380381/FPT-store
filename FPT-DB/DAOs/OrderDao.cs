ff9c5097c98ba186aad0fbb9c55f7b1c8e3feb81
LucasTran380381 <trannhan380381@gmail.com>
2020-11-11 07:54:28 +0700

    create AccessDenied.cshtml

--- FPT-DB/DAOs/OrderDao.cs
+++ FPT-DB/DAOs/OrderDao.cs
@@ -26,5 +26,27 @@ namespace FptDB.DAOs
 
             return dto;
         }
+
+
+        public OrderDto Get(string id)
+        {
+            OrderDto dto;
+            using (var connection = DbUtil.GetConn())
+            {
+                var sql =
+                    @"select id Id, total Total, date Date, fk_accounts Email, fk_status StatusId
+                from orders
+                where id = @Id
+                and fk_status = 1";
+
+                var param = new
+                {
+                    Id = id
+                };
+                dto = connection.QuerySingleOrDefault<OrderDto>(sql, param);
+            }
+
+            return dto;
+        }
     }
 }
